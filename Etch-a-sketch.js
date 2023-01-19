2399af7e5dc46836adbd948b8cdf0ffd6b3a8989
Ashtonmey <ashcalebmeyer@gmail.com>
2023-01-19 23:58:01 +0200

    Completed extra credit exercise

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -7,6 +7,7 @@ button.addEventListener('click', setGridSize);
 // set the default grid size to 4
 
 gridSize = 4;
+saturation = 100;
 
 // call function to build the initial grid of the default size
 
@@ -31,25 +32,41 @@ for (i = 0; i<gridSize; i++)
     {
         const div_cell = document.createElement("div");
         div_cell.classList.add("cell");
-        div_cell.classList.add("gird-element");
-        div_cell.setAttribute("id", `cell${i*4+j}`);
-        div_cell.setAttribute(`style`, `height: ${800/gridSize}px;`);
+        div_cell.classList.add("grid-element");
+        div_cell.setAttribute("id", `cell${i*4+j}`);        
+        div_cell.setAttribute(`style`, `height: ${800/gridSize}px; background-color: hsl(${Math.round(Math.random()*360)},100%,90%);`);                    
         div_row.appendChild(div_cell);
-        div_cell.addEventListener('mouseover', function(e) {
-        this.classList.add("seen");
+        div_cell.addEventListener('mouseover', function(e) {        
+        let cellColor = this.style.backgroundColor;
+        this.setAttribute(`style`, `height: ${800/gridSize}px; background-color: ${changeColor(cellColor)}`);
         });
     }
 }
 return;
 }
 
+
+
 function setGridSize()
 {
     gridSize = prompt("Enter the grid size required.");
+    if (gridSize > 100) { gridSize = 100;}
 
     const container = document.querySelector(".container");
     container.textContent="";
 
     buildGrid();
     return;
+}
+
+function changeColor (cellColor)
+{
+    console.log(cellColor);
+    let colors = cellColor.replace("rgb(","").replace(")","").split(',');
+    console.log(colors[2]);
+    let r = +colors[0]-60;
+    let g = +colors[1]-60;
+    let b = +colors[2]-60;
+    cellColor = `rgb(${r},${g},${b})`;
+    return cellColor;
 }
